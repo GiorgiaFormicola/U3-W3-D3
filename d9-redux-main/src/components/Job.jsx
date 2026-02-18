25530f9a0cce21bb595a53c325ca4fb3b7e0656b
GiorgiaFormicola <giorgia.formicola97@gmail.com>
2026-02-18 15:53:48 +0100

    First commit + Main search and Job components updated

---
+++ d9-redux-main/src/components/Job.jsx
@@ -0,0 +1,44 @@
+import { Row, Col } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+
+import { useSelector } from "react-redux";
+
+const Job = ({ data }) => {
+  const dispatch = useDispatch();
+  const favouritesList = useSelector((currentState) => currentState.favourites.list);
+
+  return (
+    <Row className="mx-0 mt-3 p-3 align-items-center" style={{ border: "1px solid #00000033", borderRadius: 4 }}>
+      <Col xs={1}>
+        <i
+          className={favouritesList.includes(data.company_name) ? "bi bi-suit-heart-fill text-danger" : "bi bi-suit-heart text-secondary text-opacity-50"}
+          style={{ cursor: "pointer" }}
+          onClick={() => {
+            if (!favouritesList.includes(data.company_name)) {
+              dispatch({
+                type: "ADD_TO_FAVOURITES",
+                payload: data.company_name,
+              });
+            } else {
+              dispatch({
+                type: "REMOVE_FROM_FAVOURITES",
+                payload: data.company_name,
+              });
+            }
+          }}
+        ></i>
+      </Col>
+      <Col xs={3} className="border-end">
+        <Link to={`/${data.company_name}`}>{data.company_name}</Link>
+      </Col>
+      <Col xs={7}>
+        <a href={data.url} target="_blank" rel="noreferrer">
+          {data.title}
+        </a>
+      </Col>
+    </Row>
+  );
+};
+
+export default Job;
